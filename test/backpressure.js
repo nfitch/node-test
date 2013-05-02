555bc619bb60703aa3e429bc94b298bed1d03adb
Nate Fitch <nate.fitch@joyent.com>
2013-05-02 09:08:16 -0700

    Adding demux to backpressure.

--- test/backpressure.js
+++ test/backpressure.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
 
-//http://nodejs.org/docs/v0.8.22/api/stream.html
-
+/**
+ * Note that this is irrelevant if you're working with node > 0.10 since that's
+ * the rev where the streams interface changed.  See:
+ *     http://blog.nodejs.org/2013/03/11/node-v0-10-0-stable/
+ *     https://github.com/isaacs/readable-stream
+ *
+ * For the < 0.10 docs (and what the streams below are made for) see:
+ *     http://nodejs.org/docs/v0.8.22/api/stream.html
+ */
 var Stream = require('stream').Stream;
 var util = require('util');
 //module.exports = ReadableStream;
@@ -56,7 +63,6 @@ FastReadableStream.prototype.resume = function() {
 
 FastReadableStream.prototype.destroy = function() {
         this.log('destroyed');
-        this.pause();
         this.readable = false;
         this.emit('end');
         this.emit('close');
@@ -105,6 +111,7 @@ SlowWritableStream.prototype.write = function(data) {
 
 SlowWritableStream.prototype.end = function() {
         this.log('close');
+        this.writable = false;
         this.emit('close');
 };
 
@@ -118,9 +125,119 @@ SlowWritableStream.prototype.log = function(message) {
         }
 }
 
+//Demux stream
+function demux(readstream, writestreams, cb) {
+        var i = 0;
+        readstream.pause();
+        var waitingForDrain = [];
+        var ended = false;
+
+        function tryEnd() {
+                if (ended) {
+                        return;
+                }
+                var endNow = !readstream.readable;
+                for (var i = 0; i < writestreams.length; ++i) {
+                        endNow = endNow && !writestreams[i].writable;
+                }
+                if (endNow) {
+                        ended = true;
+                        cb();
+                }
+        }
+
+        //Set up the read stream
+        readstream.on('data', function (chunk) {
+                //Round robin
+                var ws = writestreams[i++ % writestreams.length];
+                if (false === ws.write(chunk)) {
+                        if (waitingForDrain.indexOf(ws) === -1) {
+                                waitingForDrain.push(ws);
+                        }
+                        readstream.pause();
+                }
+        });
+
+        readstream.on('end', function () {
+                //In a real library, should probably have an option for
+                // leaving these open.
+                for (var i = 0; i < writestreams.length; ++i) {
+                        writestreams[i].end();
+                }
+        });
+
+        readstream.on('error', function (err) {
+                err.stream = readstream;
+                cb(err);
+        });
+
+        readstream.on('close', function () {
+                tryEnd();
+        });
+
+        //Set up each write stream
+        for (var i = 0; i < writestreams.length; ++i) {
+                var ws = writestreams[i];
+                ws.on('drain', function () {
+                        var s = this;
+                        var index = waitingForDrain.indexOf(s);
+                        if (index !== -1) {
+                                waitingForDrain.splice(index, 1);
+                        }
+                        if (waitingForDrain.length === 0) {
+                                readstream.resume();
+                        }
+                }.bind(ws));
+
+                ws.on('error', function (err) {
+                        err.stream = this;
+                        cb(err);
+                }.bind(ws));
+
+                ws.on('close', function () {
+                        if (readstream.readable) {
+                                //Write streams shouldn't close prematurely
+                                var err = new Error(
+                                        'write stream closed prematurely');
+                                err.stream = this;
+                                cb(err);
+                                return;
+                        }
+                        tryEnd();
+                }.bind(ws));
+        }
+
+        readstream.resume();
+}
+
 
 //--- Testing
 
+
+function testDemux() {
+        console.log('Testing demux');
+        var rstream = new FastReadableStream(5);
+        var wstream1 = new SlowWritableStream(20);
+        var wstream2 = new SlowWritableStream(40);
+
+        VERBOSE = true;
+        demux(rstream, [wstream1, wstream2], function (err) {
+                if (err) {
+                        console.error(err);
+                }
+                console.log({
+                        'rstream.readable': rstream.readable,
+                        'wstream1.writable': wstream1.writable,
+                        'wstream2.writable': wstream2.writable
+                });
+                VERBOSE = false;
+        });
+}
+testDemux();
+
+
+//Test that the classes above work with pipe and that no pause/resume/drain is
+// called when the slow guy isn't actually slow.
 function testFastWritableStream() {
         console.log('Testing fast writable stream');
         var rstream = new FastReadableStream(100000);
@@ -141,6 +258,8 @@ function testFastWritableStream() {
 }
 //testFastWritableStream();
 
+//Testing that the classes above actually work as pausable/resumable/drinable
+// streams.
 function testSlowWritableStreamPipe() {
         console.log('Testing slow writable stream with a pipe');
         var rstream = new FastReadableStream(100);
@@ -171,7 +290,8 @@ function testSlowWritableStreamPipe() {
 }
 //testSlowWritableStreamPipe();
 
-//Checking that pausing, resuming and destroying works:
+//Checking that pausing, resuming and destroying works on the readable
+// stream.
 function testFastReadableStream() {
         var stream = new FastReadableStream();
         stream.on('data', function (data) {
---
+++ test/bind.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+function c(x) {
+        console.log(x);
+}
+
+//This won't work...
+var funcs = [];
+for (var i = 0; i < 10; ++i) {
+        funcs.push(function () {
+                console.log(i);
+        });
+}
+
+for (var j = 0; j < 10; ++j) {
+        funcs[j]();
+}
+
+//But this does...
+var funcs = [];
+for (var i = 0; i < 10; ++i) {
+        funcs.push(function (i) {
+                console.log(i);
+        }.bind(null, i));
+}
+
+for (var j = 0; j < 10; ++j) {
+        funcs[j]();
+}
